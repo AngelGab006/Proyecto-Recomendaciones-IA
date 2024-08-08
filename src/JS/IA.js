12cfbadde627286e4ca661f587394dc4840dcb1c
AngelGab006 <angelbeltrea.20@gmail.com>
2024-08-08 00:06:15 -0400

    final changes

--- src/JS/IA.js
+++ src/JS/IA.js
@@ -1,26 +1,25 @@
 import { CreateMLCEngine } from "@mlc-ai/web-llm";
+import image from "../../public/Loading.gif";
 
 async function analizar(user, frequentSongs) {
     const mensajeFinal = `Hola ${user}, basado en las canciones que escuchas frecuentemente, creemos que seguro vas a estar encantado de escuchar estas canciones:`;
   
     const messages = [
-        { role: "system", content: "You are an AI music recommendation generator. Your task is to suggest music tracks to the user based on their listening history. The user will provide a list of songs they currently enjoy. Analyze these songs to understand the user's preferences and recommend 10 new songs they might like. Your output should be a simple list of song titles followed by their artists, in Spanish. Do not include any additional text or explanation." },
+        { role: "system", content: "You are an AI music recommendation generator. Your task is to suggest music tracks to the user based on their listening history. The user will provide a list of songs they currently enjoy. Analyze these songs to understand the user's preferences and recommend 10 real songs they might like. Your output should be a simple list of song titles followed by their artists. Do not include any additional text or explanation." },
         { role: "user", content: `These are the frequent songs of the user: ${frequentSongs.join(", ")}` },
     ];
   
     try {
         const initProgressCallback = (initProgress) => {
             console.log(initProgress);
-            document.getElementById('output').innerText = initProgress.text;
+            document.getElementById('output').innerHTML =`<p id="loading"><img src="${image}" alt="loading"> ${initProgress.text}</p>`;
         };
-        
-        const selectedModel = "gemma-2-2b-it-q4f32_1-MLC-1k";
-        
-        const engine = await CreateMLCEngine(
-            selectedModel,
-            { initProgressCallback: initProgressCallback }, // engineConfig
-            { max_gpu_memory_fraction: 0.7 },
-        );
+            const selectedModel = "gemma-2-2b-it-q4f32_1-MLC-1k";
+            const engine = await CreateMLCEngine(
+                selectedModel,
+                { initProgressCallback: initProgressCallback }, // engineConfig
+                { max_gpu_memory_fraction: 0.75 },
+            );
 
         // Handle potential engine initialization issues
         if (!engine || !engine.chat || !engine.chat.completions) {
